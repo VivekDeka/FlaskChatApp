4a44ab73d023e980621a5117365c32a0c251627f
Vivek Deka <VivekDeka@users.noreply.github.com>
2025-08-05 00:14:17 +0530

    Delete Examples directory

--- Examples/example3.py
+++
@@ -1,5 +0,0 @@
-from textblob import TextBlob
-
-text =  TextBlob("I absolutely love learning Python with AI")
-
-print("Sentiment polarity", text.sentiment)
--- Examples/example4.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyCWwDKIkDUx5namNldIyUhPn4xVfPeKFGk")
-
-model = genai.GenerativeModel(model_name = "gemini-1.5-flash")
-
-response = model.generate_content("Suggest 3 innovative SaaS ideas using Python and AI")
-
-print("Gemini Response:", response.text)
